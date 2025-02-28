e072f00c0d5af30e0497b2ff820b7dd9f63f236a
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-02-28 14:10:35 +0100

    Update start_lio.sh
    added start script for lio

--- start-scripts/start_lio.sh
+++ start-scripts/start_lio.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-
+cd ~/ros2_ws
+colcon build --packages-select livox_lio
+source ./install/setup.bash
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+ros2 launch lio_livox mid360_lio_launch.py
