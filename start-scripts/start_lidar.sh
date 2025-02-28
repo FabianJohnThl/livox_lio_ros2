7f18fba5db0b48b37f00c20f38a701295412bf46
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-02-28 14:11:05 +0100

    Update start_lidar.sh
    changed path to home

--- start-scripts/start_lidar.sh
+++ start-scripts/start_lidar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/fiveg/ros2_ws
+cd ~/ros2_ws
 colcon build --packages-select livox_ros_driver2 
 source ./install/setup.bash
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
