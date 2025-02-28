0cf5bc14eca13c1471945bde281e1f6299e0e6c9
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-02-28 14:24:18 +0100

    Update mid360_lio_launch.py
    fixed minor bugs

--- launch/mid360_lio_launch.py
+++ launch/mid360_lio_launch.py
@@ -1,12 +1,14 @@
+import os
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
+import launch
 
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
 cur_rviz_path = cur_path + '../rviz_cfg'
-rviz_config_path = os.path.join(cur_config_path, 'mid360_lio_ros2.rviz')
+rviz_config_path = os.path.join(cur_rviz_path, 'mid360_lio_ros2.rviz')
 
 def generate_launch_description():
     ScanRegistration = Node(
