8986b84209c866cc26d86f8fac1beb401ea42d29
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-02-28 14:08:26 +0100

    Update start_lidar.sh
    added start script for the lidar

--- start-scripts/start_lidar.sh
+++ start-scripts/start_lidar.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-
+cd /home/fiveg/ros2_ws
+colcon build --packages-select livox_ros_driver2 
+source ./install/setup.bash
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+ros2 launch livox_ros_driver2 MID360_LIO_launch.py
