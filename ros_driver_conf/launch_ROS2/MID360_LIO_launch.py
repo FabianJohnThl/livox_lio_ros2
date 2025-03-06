e700bbea219e566e374b9b73d058f86a46fac149
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-03-06 08:33:08 +0100

    Update MID360_LIO_launch.py
    added hint for dependency between publish_freq and scan_time of pointcloud_to_laserscan launch file parameter

--- ros_driver_conf/launch_ROS2/MID360_LIO_launch.py
+++ ros_driver_conf/launch_ROS2/MID360_LIO_launch.py
@@ -8,6 +8,7 @@ import launch
 xfer_format   = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
+# check, that publish_freq matches to the ../launch/launch_pc2ls_launch.py files scan_time value 
 publish_freq  = 5.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
 output_type   = 0
 frame_id      = 'livox_frame'
