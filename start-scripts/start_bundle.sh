f370c93e3d1bd4b64af0ccd954298912d60bde58
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-03-06 08:46:30 +0100

    Update start_bundle.sh
    added pointcloud_to_laserscan and slam by gmapping

--- start-scripts/start_bundle.sh
+++ start-scripts/start_bundle.sh
@@ -4,11 +4,15 @@ cd ~
 
 screen -XS lio quit
 screen -XS lidar quit
+screen -XS slam quit
+screen -XS pc2ls quit
 
 sleep 1
 
 screen -dmS lidar bash ./start_lidar.sh
 screen -dmS lio bash ./start_lio.sh
+screen -dmS slam bash ./start_slam.sh
+screen -dmS pc2ls bash ./start_pc2ls.sh
 
 sleep 2
 
