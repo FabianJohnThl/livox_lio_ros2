fa699698721106e924a681fd99bc274d2a6e6c56
FabianJohnThl <107192633+FabianJohnThl@users.noreply.github.com>
2025-03-06 09:24:27 +0100

    Update start_slam.sh
    fixed path to the correct file

--- start-scripts/start_slam.sh
+++ start-scripts/start_slam.sh
@@ -4,4 +4,4 @@ cd ~/ros2_ws
 colcon build --packages-select slam_gmapping 
 source ./install/setup.bash
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-ros2 launch slam_gmapping slam_gmapping.launch.py
+ros2 launch slam_gmapping ./slam_gmapping_launch.py
